f9395f0c024c15d2a30d39bc88095a7d2d3a8ab8
Hugogp <hugogp@hotmail.es>
2024-03-12 19:24:20 +0100

    feat: test for user and homedetector class

---
+++ test/test_home_detector.py
@@ -0,0 +1,39 @@
+import unittest
+import numpy as np
+
+from src.app.user.home_detector import HomeDetector
+
+
+class TestLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.detector = HomeDetector()
+
+    def test_predict_returns_list_of_tuples(self):
+        image = "test/data/input_example.png"
+        result = self.detector.predict(image)
+
+        self.assertIsInstance(result, list)
+
+        for bbox, score in result:
+            self.assertIsInstance(bbox, np.ndarray)
+            self.assertIsInstance(score, float)
+
+    def test_predict_returns_correct_format(self):
+        image = "test/data/input_example.png"
+        result = self.detector.predict(image)
+
+        for bbox, score in result:
+            self.assertEqual(len(bbox), 4)
+            self.assertGreaterEqual(score, 0.0)
+            self.assertLessEqual(score, 100.0)
+
+    def test_predict_handles_empty_image(self):
+        empty_image = np.zeros((100, 100, 3), dtype=np.uint8)
+        result = self.detector.predict(empty_image)
+
+        self.assertEqual(len(result), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_login.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+from src.app.auth.models import User
+
+
+class TestLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.user = User()
+
+    def test_login_successful(self):
+        self.assertTrue(self.user.verify_password("password1"))
+
+    def test_login_failed_incorrect_password(self):
+        self.assertTrue(self.user.verify_password("wrong_password"))
+
+
+if __name__ == '__main__':
+    unittest.main()
