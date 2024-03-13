1802902cd66de645aaf02fcef2d1d048269222d0
Hugogp <hugogp@hotmail.es>
2024-03-13 17:35:08 +0100

    feat: more test for predict

--- test/test_house_detector.py
+++ test/test_house_detector.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2 as cv
 
 from src.app.user.house_detector import HouseDetector
+from src.house_detector.utils.io_utils import get_bounding_boxes
 
 
 class TestHouseDetector(unittest.TestCase):
@@ -11,7 +12,7 @@ class TestHouseDetector(unittest.TestCase):
         self.detector = HouseDetector()
 
     def test_predict_returns_list_of_tuples(self):
-        image = "test/data/input_example.png"
+        image = "test/data/austin1.tif"
         result = self.detector.predict(image)
 
         self.assertIsInstance(result, list)
@@ -23,7 +24,7 @@ class TestHouseDetector(unittest.TestCase):
             self.assertIsInstance(score, float)
 
     def test_predict_returns_correct_format(self):
-        image = "test/data/input_example.png"
+        image = "test/data/austin1.tif"
         result = self.detector.predict(image)
 
         for bbox, score in result:
@@ -38,7 +39,7 @@ class TestHouseDetector(unittest.TestCase):
         self.assertEqual(len(result), 0)
 
     def test_predict_coordinates_within_image_size(self):
-        image = "test/data/input_example.png"
+        image = "test/data/austin1.tif"
         result = self.detector.predict(image)
 
         for bbox, _ in result:
@@ -50,6 +51,25 @@ class TestHouseDetector(unittest.TestCase):
                     self.assertLessEqual(coord, img_width)
                 else:
                     self.assertLessEqual(coord, img_height)
+
+    def test_predict_performance(self):
+        image = "test/data/austin1.tif"
+        gt = "test/data/gt_austin1.tif"
+
+        bounding_boxes_gt = get_bounding_boxes(gt)
+        bounding_boxes_pred = self.detector.predict(image)
+
+        accuracy = len(bounding_boxes_pred) / len(bounding_boxes_gt) * 100
+        self.assertGreaterEqual(accuracy, 90)
+
+    def test_predict_less_equal_groundtruth(self):
+        image = "test/data/austin1.tif"
+        gt = "test/data/gt_austin1.tif"
+
+        bounding_boxes_gt = get_bounding_boxes(gt)
+        bounding_boxes_pred = self.detector.predict(image)
+
+        self.assertLessEqual(len(bounding_boxes_pred), len(bounding_boxes_gt))
 
 
 if __name__ == '__main__':
