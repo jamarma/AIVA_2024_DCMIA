d34ef63a3da039d57950063fde54b5d9563b0700
Hugogp <hugogp@hotmail.es>
2024-04-04 13:05:09 +0200

    fix: test_evaluation correction #9

--- test/test_evaluation.py
+++ test/test_evaluation.py
@@ -1,30 +1,44 @@
 import unittest
 import cv2
-from src.evaluation import Evaluator
+import numpy as np
+from src.house_detector_evaluator import HouseDetectorEvaluator
 
 class TestEvaluator(unittest.TestCase):
     def setUp(self):
-        # Example data
-        self.mask_boxes = [(10, 10, 50, 50), (60, 60, 100, 100), (80, 80, 120, 120), (190, 190, 230, 230), (240, 240, 280, 280)]
+        self.evaluator = HouseDetectorEvaluator()
 
-        self.boxes = [(10, 10, 50, 50), (60, 60, 100, 100), (30, 30, 70, 70), (80, 80, 120, 120), (220, 220, 260, 260)]
+        # Example ground truth mask
+        self.mask = np.zeros((300, 300), dtype=np.uint8)
+        cv2.rectangle(self.mask, (10, 10), (50, 50), (255), thickness=-1)
+        cv2.rectangle(self.mask, (60, 60), (100, 100), (255), thickness=-1)
+        cv2.rectangle(self.mask, (80, 80), (120, 120), (255), thickness=-1)
+        cv2.rectangle(self.mask, (150, 150), (190, 190), (255), thickness=-1)
+        cv2.rectangle(self.mask, (220, 220), (260, 260), (255), thickness=-1)
 
-    def test_calculate_iou(self):
-        box1 = self.boxes[0]
-        box2 = self.mask_boxes[0]
-        iou = Evaluator.calculate_iou(box1, box2)
+        # Example predicted bounding boxes
+        self.boxes = np.array([
+            [10, 10, 50, 50],
+            [60, 60, 100, 100],
+            [30, 30, 70, 70],
+            [80, 80, 120, 120],
+            [110, 110, 150, 150]
+        ])
 
-        self.assertAlmostEqual(iou, 1)
+    def test_add_gt_mask(self):
+        self.evaluator.add_gt_mask(self.mask)
+        self.assertEqual(len(self.evaluator.boxes_gt), 4)
+
+    def test_evaluate_num_houses(self):
+        self.evaluator.add_gt_mask(self.mask)
+        ratio = self.evaluator.evaluate_num_houses(self.boxes)
+        self.assertAlmostEqual(ratio, 1.25)
 
     def test_evaluate_bounding_boxes(self):
-        pred_boxes = self.boxes
-        gt_boxes = self.mask_boxes
-        iou_threshold = 0.5
-        TP, FN, FP = Evaluator.evaluate_bounding_boxes(pred_boxes, gt_boxes, iou_threshold)
+        self.evaluator.add_gt_mask(self.mask)
+        AP, AR = self.evaluator.evaluate_bounding_boxes(self.boxes, 0.5)
+        self.assertAlmostEqual(AP, 0.2)
+        self.assertAlmostEqual(AR, 0.25)
 
-        self.assertEqual(TP, 3)
-        self.assertEqual(FN, 2)
-        self.assertEqual(FP, 2)
 
 if __name__ == '__main__':
     unittest.main()
